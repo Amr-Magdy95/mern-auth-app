53dc36580ca7e17e357524de6afb4770079a29d8
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-12-30 17:05:11 +0200

    Feature: Added connection to mongodb atlas

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
+require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // middleware
+app.use(express.json());
 
 // Server is now live
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, console.log(`Server is now listening on port: ${PORT}...`));
+const start = async () => {
+  try {
+    console.log("Connecting to the DB...");
+    await require("mongoose").connect(process.env.MONGO_URI);
+    console.log("Connected to the DB!");
+    app.listen(
+      PORT,
+      console.log(`Server is now listening on port: ${PORT}...`)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
