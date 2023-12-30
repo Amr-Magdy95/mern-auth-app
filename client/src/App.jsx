5a2bfc14054bf269194233a2b217a8584bfa683a
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-12-30 16:14:53 +0200

    added routes and empty pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./shared/pages/Home";
+import About from "./shared/pages/About";
+import Signin from "./users/pages/Signin";
+import Signup from "./users/pages/Signup";
+import Profile from "./users/pages/Profile";
+
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
