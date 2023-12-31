ca82c505cf121934393dbd7be19625a17b0d9e31
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-12-31 19:44:44 +0200

    Feature: Backend - Completed Signup functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,15 +6,23 @@ const { validationResult, matchedData } = require("express-validator");
 exports.signup = async (req, res) => {
   // validate data received in request body
   const validationRes = validationResult(req);
-  console.log(validationRes.array());
   if (!validationRes.isEmpty())
     throw new CustomAPIError(
       validationRes.array().map((res) => res.msg),
       StatusCodes.BAD_REQUEST
     );
-
   let data = matchedData(req);
 
   // check whether user is already registered
-  // if new user, register and hash password
+  const { username, email, password } = data;
+  const foundEmail = await User.findOne({ email }).exec();
+  const foundUsername = await User.findOne({ username }).exec();
+  if (foundEmail || foundUsername)
+    throw new CustomAPIError(
+      "Username and email must be unique",
+      StatusCodes.CONFLICT
+    );
+  // if no user, register and hash password
+  const newUser = await User.create(data);
+  res.status(StatusCodes.CREATED).json(newUser);
 };
--- api/middleware/ErrorHandling.js
+++ api/middleware/ErrorHandling.js
@@ -3,5 +3,5 @@ module.exports = (err, req, res, next) => {
   if (!err instanceof CustomAPIError)
     (err.code = 500), (err.message = "Something went wrong");
 
-  res.status(err.code).json({ message: err.message });
+  res.status(500).json({ message: err.message });
 };
